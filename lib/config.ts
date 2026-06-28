b139cca79d3043dd998e788dcf059da6e4ae83a4
yuhao135246-byte <yuhao135246@gmail.com>
2026-06-28 19:26:34 +0800

    Describe your changes

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
 export const PAYMENT_LINKS = {
-  wechat: "#付款:Yoel(ZYCZYC103)/收款/001",
+  wechat: "#付款:Cadence咖啡生活(ZYCZYC103)/收款/001",
   alipay: "https://qr.alipay.com/fkx18625tfksnibeyzrg74d"
 } as const;
